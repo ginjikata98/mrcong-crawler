c29fc763b5bf601af210e18cf5600881811330bb
Tony Vu <minhdvu98@gmail.com>
2019-11-15 22:38:08 +0700

    update old post

--- update-opened-posts.py
+++ update-opened-posts.py
@@ -6,32 +6,22 @@ import aiohttp
 import bs4
 import requests
 
-URL = 'https://mrcong.com/page/'
-WEB_URL = 'http://localhost:5000/api'
-MAX_PAGE = 3
+URL = 'https://dopaminegirls.com/page/'
+WEB_URL = 'http://localhost:8080/api'
+MAX_PAGE = 20
 global hed
 
 
 async def get_all_links(url, session):
     async with session.get(url) as response:
         soup = bs4.BeautifulSoup(response.content._buffer[0].decode("utf-8"), features='html.parser')
-        a_links = soup.select('.post-thumbnail a')
+        a_links = soup.select('.entry-title a')
         for link in a_links:
-            async with session.get(link.get('href')) as res:
-                soup2 = bs4.BeautifulSoup(res.content._buffer[0].decode("utf-8"), features='html.parser')
-                title = soup2.select('.post-title span')
-                download_link = soup2.select('.shortc-button')
-                print(title[0].text)
-                print(download_link[0].get('href'))
-                new_post = {
-                    "title": title[0].text,
-                    "donwloadLink": download_link[0].get('href'),
-                    "isOpen": False,
-                }
-                try:
-                    requests.post(f"{WEB_URL}/posts", json=new_post, headers=hed)
-                except Exception:
-                    pass
+            post_title = link.text.split(':')[0]
+            try:
+                requests.get(f"{WEB_URL}/posts/is-open?title={post_title}", headers=hed)
+            except Exception:
+                pass
 
 
 async def download_all_sites(sites):
@@ -58,6 +48,6 @@ if __name__ == "__main__":
     links = []
     for i in range(1, MAX_PAGE):
         links.append(f"{URL}{i}")
-    asyncio.get_event_loop().run_until_complete(download_all_sites(links))
+    asyncio.run(download_all_sites(links))
     duration = time.time() - start_time
     print(f"Downloaded {MAX_PAGE} in {duration} seconds")
